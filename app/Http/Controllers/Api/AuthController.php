8b7ba1369e1a037983da3edc0bff951e1d0397b1
lipka <lipka.lipkin@gmail.com>
2020-02-21 15:12:33 +0300

    laravel passport

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Api\Auth\AuthLoginRequest;
+use App\Http\Requests\Api\Auth\AuthRegisterRequest;
+use App\Http\Resources\Api\UserResource;
+use App\User;
+
+class AuthController extends Controller
+{
+
+    public function register(AuthRegisterRequest $request)
+    {
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password)
+        ]);
+        return UserResource::make($user->makeToken());
+    }
+
+    public function login(AuthLoginRequest $request)
+    {
+        $data = $request->only('email', 'password');
+        if (!auth()->attempt($data))
+        {
+            $error = [
+                ['message'=>'The given data was invalid.'],
+                ['errors' => ['credentials' => 'incorrect credentials']]
+            ];
+            return response($error, 422);
+        }
+        $user = auth()->user();
+        return UserResource::make($user->makeToken());
+    }
+}
--- app/User.php
+++ app/User.php
@@ -37,4 +37,10 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function makeToken()
+    {
+        $this->token = $this->createToken('authToken')->accessToken;
+        return $this;
+    }
 }
